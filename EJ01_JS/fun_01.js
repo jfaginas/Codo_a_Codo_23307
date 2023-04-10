649fa3743b40e4a1a640593fb2a64ec4efb2e519
jfaginas <jfaginas@gmail.com>
2023-04-10 20:21:43 -0300

    Add files via upload
    Commit 01

---
+++ EJ01_JS/fun_01.js
@@ -0,0 +1,12 @@
+// function sayHello(){
+//     let you = prompt("Cual es tu nombre? ");
+//     document.write("Hola"+ " " + you + "!");
+// }
+// sayHello();
+
+function combinarNombres(nombre, apellido) {
+    // console.log (nombre + " " + apellido);
+    console.log(`${nombre} ${apellido}`)
+}
+
+combinarNombres("Jose", "Faginas");
